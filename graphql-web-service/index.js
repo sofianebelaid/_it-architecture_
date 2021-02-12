82bfb39d2c544be25d139b692b3cc95e88bb3647
Sofiane Belaïd <sofianebelaid@Mac-mini-de-Sofiane.local>
2021-02-12 10:36:46 +0100

    graphql

---
+++ graphql-web-service/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+ 
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    helloworld(name : String) : String
+  }
+`);
+ 
+// The root provides a resolver function for each API endpoint
+var root = {
+  helloworld: ({name}) => {
+    return 'Hello ' + name;
+  },
+};
+ 
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
